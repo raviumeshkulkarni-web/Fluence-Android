78943e58b883782d17379de1488b3da0f64368ed
Ravi Umesh Kulkarni <raviumeshkulkarni@gmail.com>
2026-07-19 18:37:57 +0530

    Bump version to 1.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 11
-        versionName = "1.3.5"
+        versionName = "1.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
