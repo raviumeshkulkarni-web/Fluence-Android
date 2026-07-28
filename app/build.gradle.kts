e40b8dabdf0237deed235938372f9d7762288cdd
Ravi Umesh Kulkarni <raviumeshkulkarni@gmail.com>
2026-07-29 00:09:57 +0530

    chore: bump version to 1.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.groq.voicetyper"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.6.1"
+        versionCode = 17
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
