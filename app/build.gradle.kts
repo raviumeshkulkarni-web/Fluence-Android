f4917b59b916ecc3caa2641cb66841f9e8ed106d
Ravi Umesh Kulkarni <raviumeshkulkarni@gmail.com>
2026-08-02 15:43:16 +0530

    release: bump versionCode to 22 / versionName to 1.7.0 (advisor G1 blocker)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.groq.voicetyper"
         minSdk = 26
         targetSdk = 34
-        versionCode = 21
-        versionName = "1.6.6"
+        versionCode = 22
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
